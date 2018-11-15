1bf4fb11a9ff973eda9337b90d1480240a50c73b
cy0926 <840473997@qq.com>
2018-11-15 15:07:46 +0800

    意见反馈列表不显示照片

--- src/views/check-meeting/meeting-list.vue
+++ src/views/check-meeting/meeting-list.vue
@@ -309,14 +309,14 @@ export default {
         this.searchItems.status.miss = true
         this.searchItems.auditAt.miss = true
         this.searchItems.auditName.miss = true
+        delete this.filter.auditAt
+        delete this.filter.auditName
+        this.searchItems.auditName.value = ''
+        this.searchItems.auditAt.value = ''
+        this.searchItems.status.value = ''
       }
       else {
         delete this.filter.status
-        delete this.filter.auditName
-        delete this.filter.auditAt
-        this.searchItems.status.value = ''
-        this.searchItems.auditName.value = ''
-        this.searchItems.auditAt.value = ''
         this.searchItems.status.miss = false
         this.searchItems.auditName.miss = false
         this.searchItems.auditAt.miss = false
@@ -355,6 +355,7 @@ export default {
     },
     onSearch() {
       if (helper.isEmptyObject(this.sortObj)) {
+        console.log(this.filter)
         this.filter = Object.assign(this.filter, this.sortObj)
       }
       else {
