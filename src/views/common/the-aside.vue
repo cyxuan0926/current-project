b2b0a5036561d84c3af945ff9266357f20f1a971
twmay006 <twmay006@163.com>
2018-12-14 18:15:59 +0800

    aside

--- src/views/common/the-aside.vue
+++ src/views/common/the-aside.vue
@@ -4,13 +4,12 @@
       <img
         src="/static/images/user2-160x160.jpg"
         class="avatar circle">
-      <div
-        :class="['info', 'over-hidden', {'disappear' : isCollapsed}]">
+      <div class="info">
         <span
           class="bold ellipsis"
-          v-if="Number(user.role)">{{ user.jailName }}{{ user.jailName }}{{ user.jailName }}</span>
-        <div>
-          <span class="bold">{{ user.role | role }}</span>
+          v-if="Number(user.role)">{{ user.jailName }}</span>
+        <div class="ellipsis">
+          <span class="bold1">{{ user.role | role }}</span>
           <el-popover
             placement="bottom"
             width="260"
@@ -18,74 +17,52 @@
             :disabled="prisonerAreas.length <= 1"
             :content="prisonerAreas.join('、')">
             <span
-              class="bold ellipsis"
-              slot="reference">dddddddddd{{ prisonerAreas[0] }}</span>
+              class="bold1 ml-10"
+              slot="reference">{{ prisonerAreas[0] }}</span>
           </el-popover>
         </div>
       </div>
     </div>
+    <el-menu
+      :default-active="$route.meta.deep ? getActiveMenu() : $route.path"
+      background-color="#222d32"
+      text-color="#b8c7ce"
+      active-text-color="#fff"
+      :collapse="isCollapsed"
+      class="first-level"
+      unique-opened
+      @select="handleSelect">
+      <template v-for="(item) in menu">
+        <el-menu-item
+          v-if="!item.children"
+          class="border-bottom"
+          :key="item.path"
+          :index="item.path">
+          <i :class="item.icon" />
+          <span slot="title">{{ item.title }}</span>
+        </el-menu-item>
+        <el-submenu
+          v-if="item.children"
+          :class="[
+            'border-bottom',
+            { 'is-active': item.children.indexOf(r => r.path === $route.path) > -1 }
+          ]"
+          :key="item.path"
+          :index="item.path">
+          <template slot="title">
+            <i :class="item.icon" />
+            <span>{{ item.title }}</span>
+          </template>
+          <template v-for="second in item.children">
+            <el-menu-item
+              v-if="!second.hidden"
+              :key="second.path"
+              :index="second.path">{{ second.title }}</el-menu-item>
+          </template>
+        </el-submenu>
+      </template>
+    </el-menu>
   </div>
-  <!-- <aside
-    id="main-sidebar"
-    class="main-sidebar">
-    <section
-      class="sidebar"
-      style="height: auto;">
-      <div class="user-panel">
-        <div class="pull-left image">
-          <img
-            src="../../../static/dist/img/user2-160x160.jpg"
-            class="img-circle"
-            alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p v-if="Number(user.role)">{{ user.jailName }}</p>
-          <p :class="{'super-role': !Number(user.role)}">
-            <span>{{ user.role | role }}</span>
-            <el-tooltip
-              v-if="prisonerAreas.length"
-              :disabled="prisonerAreas.length === 1"
-              popper-class="prisonser_areas__popper"
-              :content="prisonerAreas.join('、')"
-              placement="bottom">
-              <span style="margin-left: 5px">{{ prisonerAreas[0] }}</span>
-            </el-tooltip>
-          </p>
-        </div>
-      </div>
-      <ul class="sidebar-menu">
-        <li class="header">导航</li>
-        <li
-          v-for="(first, index) in menus[user.role]"
-          :key="index">
-          <template v-if="!first.children">
-            <router-link :to="first.path">
-              <i :class="first.icon"/>
-              <span>{{ first.title }}</span>
-            </router-link>
-          </template>
-          <template v-else>
-            <a
-              href="javascript:;"
-              @click="onTreeMenuClick(first)">
-              <i :class="first.icon"/>
-              <span>{{ first.title }}</span>
-              <i
-                class="el-submenu__icon-arrow"
-                :class="first.active ? 'el-icon-arrow-down' : 'el-icon-arrow-right'"/>
-            </a>
-            <ul class="treeview-menu">
-              <li
-                v-for="(second, order) in first.children"
-                :key="order">
-                <router-link :to="second.path"><i :class="second.icon"/>&nbsp;&nbsp;{{ second.title }}</router-link>
-              </li>
-            </ul>
-          </template>
-        </li>
-      </ul>
-    </section>
-  </aside> -->
 </template>
 
 <script>
@@ -95,7 +72,7 @@ import menu from './menu.js'
 export default {
   data() {
     return {
-      menus: menu,
+      menu: [],
       prisonerAreas: []
     }
   },
@@ -111,13 +88,15 @@ export default {
         return pre.concat([cur.prisonConfigName])
       }, [])
     }
+    this.menu = menu[Number(this.user.role)]
   },
   methods: {
-    onTreeMenuClick(e) {
-      this.menus[this.user.role].forEach(item => {
-        if (e.path !== item.path) item.active = false
-      })
-      e.active = !e.active
+    getActiveMenu() {
+      let active = this.$route.path.replace(/(\/[A-z-]+)(\/[A-z-]+)*/, '$1')
+      return active
+    },
+    handleSelect(e, keyPath) {
+      this.$router.push(e)
     }
   }
 }
