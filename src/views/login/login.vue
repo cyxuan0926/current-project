a87a601a69689154f58df07d47f9ba2711212c9f
twmay006 <twmay006@163.com>
2018-12-29 17:23:31 +0800

    login

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="height100 center">
+  <div class="center">
     <div class="login-container">
       <h1>狱务公开管理平台</h1>
       <div class="login-form">
@@ -12,14 +12,18 @@
             <el-input
               v-model="formData.prison"
               placeholder="监狱编号">
-              <!-- <template slot="prepend">监狱编号</template> -->
+              <span
+                class="prepend"
+                slot="prepend">监狱编号</span>
             </el-input>
           </el-form-item>
           <el-form-item prop="username">
             <el-input
               v-model="formData.username"
               placeholder="用户名">
-              <!-- <template slot="prepend">用户名</template> -->
+              <span
+                class="prepend"
+                slot="prepend">用户名</span>
             </el-input>
           </el-form-item>
           <el-form-item prop="password">
@@ -27,15 +31,17 @@
               type="password"
               v-model="formData.password"
               placeholder="密码">
-              <!-- <template slot="prepend">密码</template> -->
+              <span
+                class="prepend"
+                slot="prepend">密码</span>
             </el-input>
           </el-form-item>
-          <el-form-item class="white-login">
-            <el-checkbox v-model="isRember">记住密码</el-checkbox>
+          <el-form-item>
+            <el-checkbox v-model="isRember"><span class="white">记住密码</span></el-checkbox>
           </el-form-item>
         </el-form>
         <el-button
-          class="button-login"
+          class="width100"
           type="primary"
           :loading="loading"
           @click="onSubmit">登录</el-button>
@@ -60,8 +66,9 @@ export default {
       rules: {
         password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
         username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-        prison: [{ required: true, message: '请输入监狱代码', trigger: 'blur' }]
-      }
+        prison: [{ required: true, message: '请输入监狱编号', trigger: 'blur' }]
+      },
+      loged: false
     }
   },
   mounted() {
@@ -78,12 +85,14 @@ export default {
   methods: {
     ...mapActions(['login', 'setCookie', 'getCookie', 'removeCookie']),
     onSubmit() {
+      if (this.loged || this.loading) return false
       this.$refs.form.validate(valid => {
         if (valid) {
           this.loading = true
           this.login(this.formData).then(res => {
             this.loading = false
             if (!res) return
+            this.loged = true
             let params = {
               password: Base64.encode(this.formData.password),
               username: this.formData.username,
@@ -102,4 +111,3 @@ export default {
 <style lang="scss" scoped>
 @import "../../assets/css/login";
 </style>
-
