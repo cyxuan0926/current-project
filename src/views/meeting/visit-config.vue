fab0b475cbfbe94cefd57ae5109737b7c6205b68
twmay006 <twmay006@163.com>
2019-01-03 16:37:21 +0800

    实地探监

--- src/views/meeting/visit-config.vue
+++ src/views/meeting/visit-config.vue
@@ -1,69 +1,53 @@
 <template>
-  <div
-    class="yt-form"
-    style="padding-top: 20px;">
-    <el-form
-      v-if="show"
-      ref="form"
-      label-width="140px"
-      :model="prisonVisitConfigDetail"
-      inline
-      style="width: 75%; margin: auto; overflow: hidden;"
-      :rules="rules">
-      <el-form-item
-        label="实地探监窗口个数"
-        prop="windowSize">
+  <div class="w80">
+    <div class="el-form-item is-required">
+      <label class="el-form-item__label">实地探监窗口个数</label>
+      <div style="float: left; width: calc(100% - 150px); position: relative;">
         <el-input
           v-model="prisonVisitConfigDetail.windowSize"
           :disabled="prisonVisitConfigDetail.canNotChange"
-          placeholder="请填写实地探监窗口个数(1-20)">
+          class="part-right"
+          size="small"
+          placeholder="请填写实地探监窗口个数">
           <template slot="append">/个</template>
         </el-input>
-      </el-form-item>
-      <div>
-        <div
-          class="el-form-item is-required"
-          style="float: left;">
-          <label
-            class="el-form-item__label"
-            style="width: 130px; padding-right: 2px;">配置</label>
-        </div>
-        <div style="float: left; width: calc(100% - 140px);">
-          <template v-for="(item, index) in prisonVisitConfigDetail.usual">
-            <el-form-item
-              :key="index"
-              :prop="'usual.' + index"
-              :rules="[{ required: true, message: '请选择实地会见批次' }]"
-              style="width: calc(25% - 10px); min-width: 140px; max-width: 350px;">
-              <m-time-range-picker
-                :val="prisonVisitConfigDetail.usual[index]"
-                :prev="prisonVisitConfigDetail.usual[index - 1]"
-                :next="prisonVisitConfigDetail.usual[index + 1]"
-                :disabled="prisonVisitConfigDetail.canNotChange"
-                type="usual"
-                @handleBlur="handleBlur" />
-            </el-form-item>
-          </template>
-          <el-button
-            v-if="!prisonVisitConfigDetail.canNotChange"
-            :disabled="!flag.canAddUsual"
-            type="primary"
-            style="margin-right: 10px; margin-bottom: 22px;"
-            @click="onAddRange('usual')">新增实地会见批次</el-button>
-          <el-button
-            v-if="!prisonVisitConfigDetail.canNotChange && prisonVisitConfigDetail.usual.length > 1"
-            style="margin-left: 0; margin-bottom: 22px;"
-            @click="onRestRange('usual')">重置实地会见配置</el-button>
-        </div>
+        <span
+          v-if="Boolean(errorMsg)"
+          class="tips">{{ errorMsg }}</span>
       </div>
+    </div>
+    <div class="el-form-item is-required">
+      <label class="el-form-item__label">配置</label>
+      <div style="float: left; width: calc(100% - 150px); overflow: hidden;">
+        <m-time-range-selector
+          v-for="(queue, o) in prisonVisitConfigDetail.queue"
+          :key="o"
+          :val="queue"
+          :disabled="prisonVisitConfigDetail.canNotChange"
+          :prev="prisonVisitConfigDetail.queue[o - 1]"
+          :next="prisonVisitConfigDetail.queue[o + 1]"
+          type="queue"
+          @handleBlur="handleBlur($event, prisonVisitConfigDetail.queue)" />
+        <el-button
+          v-if="!prisonVisitConfigDetail.canNotChange && Boolean(prisonVisitConfigDetail.queue && prisonVisitConfigDetail.queue.length && prisonVisitConfigDetail.queue[0] && prisonVisitConfigDetail.queue[prisonVisitConfigDetail.queue.length - 1][1] !== '23:59')"
+          type="primary"
+          size="mini"
+          class="button-float"
+          @click="onAddRange(prisonVisitConfigDetail.queue)">新增会见时间段</el-button>
+        <el-button
+          v-if="!prisonVisitConfigDetail.canNotChange && prisonVisitConfigDetail.queue[0]"
+          size="mini"
+          class="button-float"
+          @click="onRestQueue(prisonVisitConfigDetail)">重置会见时间段</el-button>
+      </div>
+    </div>
+    <div class="button-box">
       <el-button
-        v-if="!prisonVisitConfigDetail.canNotChange"
+        v-if="!prisonVisitConfigDetail.canNotChange && prisonVisitConfigDetail.windowSize && !errorMsg && prisonVisitConfigDetail.queue[0] !== null"
         size="small"
         type="primary"
-        class="button-add"
-        style="margin-right: 10px;"
-        @click="onSubmit">确定</el-button>
-    </el-form>
+        @click="onSubmit">更新</el-button>
+    </div>
   </div>
 </template>
 
@@ -78,95 +62,94 @@ export default {
     if (this.$route.meta.role === '0') jailId = this.$route.params.id
     else if (this.$route.meta.role === '3') jailId = JSON.parse(localStorage.getItem('user')).jailId
     return {
-      rules: {
-        windowSize: [{ required: true, message: '请填写实地探监窗口个数' }, { validator: validator.isNumber }, { validator: validator.numberRange, min: 1, max: 20 }]
-      },
-      flag: {
-        canAddUsual: true
-      },
-      usualToAdd: [],
-      routeRole: this.$route.meta.role,
-      show: false,
       jailId
     }
   },
   computed: {
-    ...mapState(['prisonVisitConfigDetail'])
+    ...mapState(['prisonVisitConfigDetail']),
+    errorMsg() {
+      if (this.prisonVisitConfigDetail.windowSize === null || this.prisonVisitConfigDetail.windowSize === undefined) {
+        return ''
+      }
+      let msg = '', handleValid = (v) => {
+        if (typeof v === 'object') {
+          if (!msg) msg = v.message
+        }
+      }
+      validator.required({ message: '请填写实地探监窗口个数' }, this.prisonVisitConfigDetail.windowSize, handleValid)
+      validator.isNumber({}, this.prisonVisitConfigDetail.windowSize, handleValid)
+      validator.numberRange({ min: 1, max: 20 }, this.prisonVisitConfigDetail.windowSize, handleValid)
+      return msg
+    }
   },
   mounted() {
-    this.render()
+    this.getPrisonVisitConfigDetail({ jailId: this.jailId })
   },
   methods: {
     ...mapActions(['getPrisonVisitConfigDetail', 'updatePrisonVisitConfig']),
     onSubmit(e) {
-      this.$refs.form.validate(valid => {
-        if (valid) {
-          let params = Object.assign({}, this.prisonVisitConfigDetail, { jailId: this.jailId })
-          this.handleQueue(params)
-          delete params.usual
-          delete params.canNotChange
-          this.updatePrisonVisitConfig(params).then(res => {
-            if (!res) return
-            this.render()
-          })
-        }
-      })
+      // kkk
     },
-    render() {
-      this.getPrisonVisitConfigDetail({ jailId: this.jailId }).then(res => {
-        this.show = true
-        if (!res) return
-        if (!this.prisonVisitConfigDetail.canNotChange) this.getNextTime('usual', this.prisonVisitConfigDetail.usual[this.prisonVisitConfigDetail.usual.length - 1])
-      })
+    handleBlur(e, queue) {
+      queue[queue.length - 1] = e
+      this.$forceUpdate()
     },
-    handleBlur(e, type) {
-      if (type !== 'special') {
-        this.prisonVisitConfigDetail[type][this.prisonVisitConfigDetail[type].length - 1] = e
-        if (type === 'usual') this.$refs.form.validateField('usual.0')
-        this.getNextTime(type, e)
+    onAddRange(queue) {
+      queue.push(this.getNextRange(queue[queue.length - 1]))
+    },
+    onRestQueue() {
+      this.prisonVisitConfigDetail.queue = [null]
+    },
+    getNextRange(e) {
+      let sh = parseInt(e[0]),
+        eh = parseInt(e[1]),
+        sm = parseInt(e[0].split(':')[1]),
+        em = parseInt(e[1].split(':')[1]),
+        dur = (eh - sh) * 60 + em - sm,
+        time = Moment(new Date(2000, 0, 1, eh, em)).add(dur, 'minutes')
+      if (time.date() !== 1) {
+        return [e[1], '23:59']
       }
-    },
-    handleQueue(params) {
-      params.batchQueue = []
-      params.usual.forEach(queue => {
-        params.batchQueue.push(`${ queue[0] }-${ queue[1] }`)
-      })
-    },
-    onAddRange(type) {
-      this.prisonVisitConfigDetail[type].push(this[`${ type }ToAdd`])
-      this.getNextTime(type, this.prisonVisitConfigDetail[type][this.prisonVisitConfigDetail[type].length - 1])
-    },
-    onRestRange(type) {
-      this.prisonVisitConfigDetail[type] = [null]
-      this.flag[`canAdd${ type.replace(/(\w)/, (v) => v.toUpperCase()) }`] = false
-    },
-    getNextTime(type, last = null) {
-      if (!this.handleCanAdd(type, last) || !last) return
-      let start = Moment(new Date(2000, 0, 1, last[0].split(':')[0], last[0].split(':')[1])),
-        end = Moment(new Date(2000, 0, 1, last[1].split(':')[0], last[1].split(':')[1])),
-        duration = end.diff(start, 'minutes'),
-        toEnd = end.add(duration, 'minutes')
-      if (toEnd.date() !== 1) {
-        this[`${ type }ToAdd`] = [last[1], '23:59']
-      }
-      else {
-        this[`${ type }ToAdd`] = [last[1], `${ toEnd.format('HH:mm') }`]
-      }
-    },
-    handleCanAdd(type, last) {
-      let flag = true
-      if (last === null) {
-        flag = false
-      }
-      else if (last[1] === '23:59') {
-        flag = false
-      }
-      this.flag[`canAdd${ type.replace(/(\w)/, (v) => v.toUpperCase()) }`] = flag
-      return flag
+      return [e[1], time.format('HH:mm')]
     }
   }
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+.w80{
+  padding: 20px 10%;
+  .el-form-item__label{
+    width: 140px;
+    line-height: 30px;
+    float: left;
+    color: #606266;
+  }
+  .button-float{
+    float: left;
+    margin-bottom: 10px;
+    margin-right: 10px;
+    margin-left: 0;
+    &+.button-float{
+      margin-right: 0;
+    }
+  }
+  .button-box{
+    padding-bottom: 20px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    clear: both;
+  }
+  .tips{
+    color: #F56C6C;
+    position: absolute;
+    left: 0;
+    top: 32px;
+    font-size: 12px;
+  }
+}
+.part-right{
+  width: 500px;
+}
 </style>
