26f11eea0b251a2e446be31829676e9c6d50f559
twmay006 <twmay006@163.com>
2019-02-11 11:20:38 +0800

    去掉no-login页面的公共组件

--- src/views/download/app-family.vue
+++ src/views/download/app-family.vue
@@ -35,7 +35,7 @@
         <i class="iconfont icon-aaa" />
       </div>
     </section>
-    <!-- <footer class="m-footer">
+    <!-- <footer class="main-footer">
       <p><strong>Copyright © 2014-2018 <a href="http://www.sinog2c.com">国科政信科技(北京)股份有限公司</a></strong> 版权所有.</p>
       <div>
         <b>版本</b> 1.0.0
