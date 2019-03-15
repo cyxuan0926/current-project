234cce68edfd09234c69de77cd67433a3402e893
twmay006 <twmay006@163.com>
2019-03-15 11:00:11 +0800

    cookie

--- src/views/login/vuex-and-service/actions.js
+++ src/views/login/vuex-and-service/actions.js
@@ -11,10 +11,8 @@ export default {
     })
   },
   setCookie: ({ commit, dispatch }, params) => {
-    dispatch('removeCookie', params)
     let expires = new Date()
     expires.setTime(expires.getTime() + 7 * 24 * 60 * 60 * 1000)
-    console.log(expires)
     Object.keys(params).forEach(key => {
       document.cookie = `${ key }=${ params[key] };expires=${ expires.toUTCString() }`
     })
