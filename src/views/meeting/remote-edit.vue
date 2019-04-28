5ee62c30c4829c7452f69a82562e6aee6a8679dd
Shen Liang <phypigpig@163.com>
2019-04-28 11:08:53 +0800

    feat: 新增家属预约远程探视需提前申请天数配置

--- src/views/meeting/remote-edit.vue
+++ src/views/meeting/remote-edit.vue
@@ -5,11 +5,18 @@
       type="border-card"
       class="min-height-400"
       @tab-click="handleClick">
+      <div v-if="activeName === 'usual'" class="remote-visit-form">
+        <remote-visit-day
+          v-model="advanceDayLimit_"
+          @submit="handleUpdateAdvanceDayLimit"
+        />
+      </div>
       <template v-for="item in tabMapOptions">
         <el-tab-pane
           :label="item.label"
           :key='item.key'
           :name="item.key">
+
           <keep-alive>
             <component
               v-if='activeName == item.key'
@@ -23,11 +30,13 @@
 </template>
 
 <script>
+import remoteVisitDay from './components/remote-visit-day'
 import usual from './components/remote-usual'
 import special from './components/remote-special'
 import times from './components/remote-times'
+import { mapActions, mapState } from 'vuex';
 export default {
-  components: { usual, special, times },
+  components: { remoteVisitDay, usual, special, times },
   data() {
     return {
       activeName: 'usual',
@@ -35,16 +44,33 @@ export default {
         { label: '常规配置', key: 'usual' },
         { label: '特殊日期配置', key: 'special' },
         { label: '每人日申请次数限制配置', key: 'times' }
-      ]
+      ],
+      advanceDayLimit_: 1 // 远程探视申请需提前天数
+    }
+  },
+  computed: {
+    ...mapState(['advanceDayLimit']),
+    jailId() {
+      return this.$route.meta.role === '3' ? JSON.parse(localStorage.getItem('user')).jailId : this.$route.params.id
     }
   },
   watch: {
-    '$route': 'render'
+    '$route': 'render',
+    advanceDayLimit: {
+      immediate: true,
+      handler(val) {
+        this.advanceDayLimit_ = val
+      }
+    }
+  },
+  created() {
+    this.getRemoteAdvanceDayLimit({ jailId: this.jailId })
   },
   mounted() {
     this.render()
   },
   methods: {
+    ...mapActions(['getRemoteAdvanceDayLimit', 'updateRemoteAdvanceDayLimit']),
     handleClick() {
       this.$router.replace({ query: { tag: this.activeName } })
     },
@@ -61,6 +87,12 @@ export default {
           this.handleClick()
         }
       }
+    },
+    handleUpdateAdvanceDayLimit(day) {
+      this.updateRemoteAdvanceDayLimit({
+        jailId: this.jailId,
+        advanceDayLimit: day
+      })
     }
   }
 }
@@ -68,5 +100,14 @@ export default {
 <style lang="scss" scoped>
 .w80{
   padding: 20px 10%;
+}
+
+.remote-visit-form {
+  border-bottom: 1px solid #dcdfe6;
+  margin-bottom: 15px;
+
+  /deep/ .el-form-item {
+    margin-bottom: 10px;
+  }
 }
 </style>
