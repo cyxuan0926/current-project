68cd3bae00d63840372bc26a774dd047b2488bfc
Shen Liang <phypigpig@163.com>
2019-04-29 14:19:21 +0800

    feat: 增加监狱会见统计详情

--- src/views/meeting-report/prison-report.vue
+++ src/views/meeting-report/prison-report.vue
@@ -3,83 +3,44 @@
     class="row-container"
     :gutter="0">
     <m-search
+      ref="search"
       :items="searchItems"
       @sizeChange="sizeChange"
-      @search="onSearch" />
+      @search="onSearch"
+    />
     <el-col :span="24">
-      <el-tabs
-        value="first"
-        type="card">
-        <el-tab-pane
-          label="监狱会见统计"
-          name="first" />
+      <el-tabs v-model="activeComponentName" type="card">
+        <template v-for="item in tabOptions">
+          <el-tab-pane :label="item.label" :name="item.name" :key="item.name">
+            <keep-alive>
+              <component :is="activeComponentName" />
+            </keep-alive>
+          </el-tab-pane>
+        </template>
       </el-tabs>
-      <el-table
-        :data="prisonReportList.contents"
-        border
-        show-summary
-        :summary-method="getSummaries"
-        style="width: 100%">
-        <el-table-column
-          prop="prisonArea"
-          label="监区" />
-        <el-table-column
-          prop="total"
-          label="会见申请次数">
-          <template slot-scope="scope">
-            {{ scope.row.total }} 次
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="finishedTotal"
-          label="会见成功次数">
-          <template slot-scope="scope">
-            {{ scope.row.finishedTotal }} 次
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="canceledTotal"
-          label="会见取消次数">
-          <template slot-scope="scope">
-            {{ scope.row.canceledTotal }} 次
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="expiredTotal"
-          label="会见过期次数">
-          <template slot-scope="scope">
-            {{ scope.row.expiredTotal }} 次
-          </template>
-        </el-table-column>
-        <el-table-column
-          prop="deniedTotal"
-          label="会见拒绝/撤回次数">
-          <template slot-scope="scope">
-            {{ scope.row.deniedTotal }} 次
-          </template>
-        </el-table-column>
-      </el-table>
     </el-col>
     <m-pagination
       ref="pagination"
-      :total="prisonReportList.total"
-      @onPageChange="getDatas" />
+      :total="tatalPage"
+      @onPageChange="getDatas"
+    />
   </el-row>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
 import Moment from 'moment'
+import profile from './prison-report-profile'
+import detail from './prison-report-detail'
+
 const startDate = Moment().subtract(1, 'months').format('YYYY-MM')
 const endDate = Moment().subtract(1, 'months').format('YYYY-MM')
+
 export default {
+  components: { profile, detail },
   data() {
     return {
-      tabNum: 'first',
-      filterInit: { // 默认查询上一个月的，筛选框初始化
-        startDate: startDate,
-        endDate: endDate
-      },
+      activeComponentName: 'profile',
       searchItems: {
         reportRange: {
           type: 'monthRangeSelector',
@@ -92,61 +53,75 @@ export default {
             max: Moment().subtract(1, 'months').format('YYYY-MM'),
             maxMonthRange: 24
           }
+        },
+        name: {
+          type: 'input',
+          label: '家属姓名',
+          miss: true
+        },
+        prisonerNumber: {
+          type: 'input',
+          label: '囚号',
+          miss: true
         }
+      },
+      tabOptions: [
+        { label: '监狱会见统计', name: 'profile' },
+        { label: '会见统计详情', name: 'detail' }
+      ],
+      filterInit: { // 默认查询上一个月的，筛选框初始化
+        startDate: startDate,
+        endDate: endDate
       }
     }
   },
   computed: {
-    ...mapState({
-      prisonReportList: state => state.prisonReportList,
-      user: state => state.global.user
-    })
+    ...mapState(['prisonReportList', 'prisonReportDetail']),
+    tatalPage() {
+      if (this.activeComponentName === 'profile') {
+        return this.prisonReportList.total
+      } else {
+        return this.prisonReportDetail.total
+      }
+    }
+  },
+  watch: {
+    activeComponentName(val) {
+      if (this.activeComponentName === 'profile') {
+        this.searchItems.name.miss = true
+        this.searchItems.prisonerNumber.miss = true
+
+        delete this.filter.name
+        delete this.filter.prisonerNumber
+      } else {
+        this.searchItems.name.miss = false
+        this.searchItems.prisonerNumber.miss = false
+      }
+      this.$refs.search.onSearch('tabs')
+      this.onSearch()
+    }
   },
   mounted() {
     this.getDatas()
   },
   methods: {
-    ...mapActions(['getPrisonReportList']),
+    ...mapActions(['getPrisonReportList', 'getPrisonReportDetail']),
     sizeChange(rows) {
       this.$refs.pagination.handleSizeChange(rows)
       this.getDatas()
     },
     getDatas() {
-      this.getPrisonReportList({ ...this.filter, ...this.pagination })
+      const params = { ...this.filter, ...this.pagination }
+
+      if (this.activeComponentName === 'profile') {
+        this.getPrisonReportList(params)
+      } else {
+        this.getPrisonReportDetail(params)
+      }
     },
     onSearch() {
       this.$refs.pagination.handleCurrentChange(1)
-    },
-    getSummaries(params) {
-      const { columns, data } = params
-      const sums = []
-      columns.forEach((column, index) => {
-        if (index === 0) {
-          sums[index] = this.user.jailName
-          return
-        }
-        const values = data.map(item => Number(item[column.property]))
-        if (!values.every(value => isNaN(value))) {
-          sums[index] = values.reduce((prev, curr) => {
-            const value = Number(curr)
-            if (!isNaN(value)) {
-              return prev + curr
-            }
-            else {
-              return prev
-            }
-          }, 0)
-          sums[index] += ' 次'
-        }
-        else {
-          sums[index] = 'N/A'
-        }
-      })
-      return sums
     }
   }
 }
 </script>
-
-<style type="text/stylus" lang="stylus" scoped>
-</style>
