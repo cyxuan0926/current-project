882f1f6b758dd2e37c515b90f97f9a50b8784da2
cy0926 <840473997@qq.com>
2020-01-07 18:24:05 +0800

    feat: 会见新增会见中状态

--- src/filters/modules/switches.js
+++ src/filters/modules/switches.js
@@ -34,7 +34,8 @@ let switches = {
     { value: 'FINISHED', label: '已完成' },
     // { value: 'TOBECANCELED', label: '待取消' },
     { value: 'EXPIRED', label: '已过期' },
-    { value: 'CANCELED', label: '已取消' }
+    { value: 'CANCELED', label: '已取消' },
+    { value: 'MEETING_ON', label: '会见中' }
   ],
   registStatus: [
     { value: 'PENDING', label: '未授权' },
