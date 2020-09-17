e785ee14c6aadc6e529618243b3235e7d27c429f
caiyang <caiyang@sinog2c.com>
2020-09-17 19:27:18 +0800

    fix: 网关错误 跳转到登录页面/去掉可视

--- src/common/constants/const.js
+++ src/common/constants/const.js
@@ -11,7 +11,7 @@ export const withdrawOrAnthorinputReason = `1、上传的资料不足以证明�
 4、根据可视可视电话管理规定，只允许直系亲属申请。`
 
 export const registrationWithdrawOrAnthorinputReason = `1、夫妻关系必须上传结婚证。
-2、根据可视可视电话管理规定，只允许直系亲属申请。`
+2、根据可视电话管理规定，只允许直系亲属申请。`
 
 // 人脸识别阈值范围
 export const faceRecognitionValues = [ '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1' ]
