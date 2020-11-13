6751b589f5f015436ddfcbebaac11a9b1e3d26a4
HUBI880626 <284432494@qq.com>
2020-11-13 18:02:27 +0800

    fix：修改路由权限校验

--- src/router/index.js
+++ src/router/index.js
@@ -72,7 +72,7 @@ router.beforeEach((to, from, next) => {
 // 路由权限校验
 router.beforeEach((to, from, next) => {
   const permission = store.state.account.authorities
-  if (permission.includes(to.meta.permission) || !to.meta.permission) {
+  if ((permission.length === 1 && permission[0] === 'all') || permission.includes(to.meta.permission) || !to.meta.permission) {
     next()
   }
   else {
