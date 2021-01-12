63ca107c0c9a14df186666de8ecd2258e40acf4e
HUBI880626 <284432494@qq.com>
2021-01-12 11:17:56 +0800

    bugfix：操作指引修改

--- src/views/operation-guide/guide-detail.vue
+++ src/views/operation-guide/guide-detail.vue
@@ -3,6 +3,9 @@
         <div class="guide-detail-title">{{guideData.content}}</div>
         <div class="guide-detail-date">更新时间：{{guideData.updatedTime}}</div>
         <div class="guide-detail-context" v-html="guideData.guide"></div>
+        <div class="guide-detail-back">
+            <el-button @click="handleBack">返回</el-button>
+        </div>
     </div>
 </template>
 
@@ -23,12 +26,38 @@
         },
         methods: {
             handleBack() {
-                this.$router.push({ path: '/operation-guide/list' })
+                this.$router.back()
             }
         }
     }
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+    .guide-detail {
+        padding: 50px 30px;
+        &-title {
+            font-size: 20px;
+            font-weight: 400;
+            color: #333;
+            text-align: center;
+            width: 70%;
+            margin: 0 auto;
+        }
+        &-date {
+            font-size: 12px;
+            color: #999;
+            text-align: center;
+            padding: 30px 0;
+        }
+        &-context {
+            padding: 16px 50px;
+            color: #666;
+            font-size: 14px;
+            line-height: 22px;
+        }
+        &-back {
+            margin-top: 30px;
+            text-align: right;
+        }
+    }
 </style>
