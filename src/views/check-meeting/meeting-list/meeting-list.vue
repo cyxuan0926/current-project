b70cbe09ffd825944218033ab62b3a17459ae626
Liujianjian <40622339@qq.com>
2021-03-09 14:33:06 +0800

    修改  驳回接口

--- src/views/check-meeting/meeting-list/meeting-list.vue
+++ src/views/check-meeting/meeting-list/meeting-list.vue
@@ -1340,7 +1340,8 @@
     },
     // 获取当前驳回原因列表
   async onRejectshow(str,isform){
-      let params=JSON.parse(localStorage.getItem('user'));
+       let params={}
+          params.jailId=JSON.parse(localStorage.getItem('user')).jailId
           params.type=2
       let res = await http.getRejectEdit( params )
       if(res.content){
