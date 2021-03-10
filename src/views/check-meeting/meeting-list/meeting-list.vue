3e1435c3c503a0259eef4518b476bb1f7f723582
Liujianjian <40622339@qq.com>
2021-03-10 16:23:34 +0800

    修改  接口名称

--- src/views/check-meeting/meeting-list/meeting-list.vue
+++ src/views/check-meeting/meeting-list/meeting-list.vue
@@ -1398,6 +1398,19 @@
         jailId:JSON.parse(localStorage.getItem('user')).jailId
         }
         let res = await http.setRejectEdit(params)
+        if(res){
+          let params={}
+              params.jailId=JSON.parse(localStorage.getItem('user')).jailId
+              params.type=2
+          let res = await http.getRejectEdit( params )
+          if(res.content){
+            this.content = res.content
+            this.contentId=res.id
+            this.updateer=res.updateEr
+          }else{
+            this.content = []
+          }
+      }
        this.show.editRebut=true
       }
     },
