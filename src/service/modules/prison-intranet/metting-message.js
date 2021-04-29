f6f4d2c4bf7c19661f168fef3c895de017e4618f
HUBI880626 <284432494@qq.com>
2021-04-29 17:18:49 +0800

    bugfix：调整代码格式

--- src/service/modules/prison-intranet/metting-message.js
+++ src/service/modules/prison-intranet/metting-message.js
@@ -1,12 +1,8 @@
 import * as service from '@/service/config/service'
 
-export default{
+export default {
       // 短信列表
-     getIntraMessagelist: params => {
-      return service.get(service.getIntraUrl('/msg/terminal/selectMessage'), params).then(res => res && res.data)
-      },
+      getIntraMessagelist: params => service.get(service.getIntraUrl('/msg/terminal/selectMessage'), params).then(res => res && res.data),
       // 短信授权
-     dealIntraMessage: params => {
-        return service.postObj(service.getIntraUrl('/msg/terminal/dealMessage'), params).then(res => res && res.data)
-      }
+      dealIntraMessage: params => service.postObj(service.getIntraUrl('/msg/terminal/dealMessage'), params).then(res => res && res.data)
 }
