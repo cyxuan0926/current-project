11428fca40b548ca84fb919c3f7e9ee0599946fb
HUBI880626 <284432494@qq.com>
2021-08-18 09:36:14 +0800

    refactor：亲情电话收费配置修改

--- src/service/urls.js
+++ src/service/urls.js
@@ -32,8 +32,7 @@ const config = {
     publicApiHost: 'http://qa-auth-api.yuwugongkai.com',
     jailApiHost: 'http://qa-ywgk.yuwugongkai.com/ywgk', // 河源监狱-qa
     // apiHost: 'http://8.129.14.169:8098', // 河源监狱
-    qaUpdate: 'http://120.78.190.101:8081/ywgk',
-    apiHost: 'http://120.78.190.101:8081',
+    apiHost: 'http://qa-ywgk.yuwugongkai.com',
     financeApiHost: 'http://qa-finance-api.yuwugongkai.com',
     monitoringApiHost: 'http://qa-monitoring-api.yuwugongkai.com',
     apiPath: '/ywgk',
