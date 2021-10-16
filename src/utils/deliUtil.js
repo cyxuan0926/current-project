10d3a5dab6a986fce583d5429fd85175ca65ba92
HUBI880626 <284432494@qq.com>
2021-10-16 16:56:22 +0800

    fix：亲情短信发送web集成得力高拍仪sdk

---
+++ src/utils/deliUtil.js
@@ -0,0 +1,96 @@
+import { Message } from 'element-ui'
+import initWebSocket from './deliWebsoket'
+
+export default class {
+    constructor(options) {
+        this.ws = null
+        this.m_isConnectWS = false
+        this.m_splitTag = '$*$'
+        this.m_closed = true
+        this.m_error = false
+        this.devIndex = 0
+        this.options = options
+    }
+
+    load() {
+        let _this = this
+        this.ws = initWebSocket({
+            onOpen() {
+                _this.m_isConnectWS = true
+                _this.unload()
+                _this.sendInit()
+                _this.sendGetPath()
+                _this.m_closed = false
+                _this.options.onWsopen()
+            },
+            onMessage(str) {
+                if (str.includes(_this.m_splitTag)) {
+                    // 视频的每一帧
+                    _this.options.onCaptureWSMessage(str.split(_this.m_splitTag)[1])
+                }
+                else {
+                    // 处理其他请求
+                    _this.handleWSMessage(str)
+                }
+            },
+            onClose() {
+                _this.m_isConnectWS = false
+                _this.m_closed = true
+            },
+            onError() {
+                _this.options.onWsError()
+                _this.m_error = true
+            }
+        })
+    }
+
+    unload() {
+        if (this.m_closed) {
+            return
+        }
+        // 反初始化
+        this.send({ FuncName: 'camUnInitCameraLib' })
+        this.ws.close()
+    }
+    // 初始化
+    sendInit() {
+        this.send({ FuncName: 'camInitCameraLib' })
+    }
+
+    // 获取电脑路径
+    sendGetPath() {
+        this.send({ FuncName: 'GetSpecialFolderPath', argument: { nFolder: 5 } })
+    }
+
+    // 打开高拍仪
+    openDev() {
+        if (this.m_closed) {
+            this.load()
+        }
+        // 打开摄像头
+        this.send({ FuncName: 'camOpenDev', argument: { devIndex: this.devIndex, subtype: 0, width: 0, height: 0 } })
+    }
+
+    // 发送ws请求
+    send(jsonObj) {
+        try {
+            this.ws.send(JSON.stringify(jsonObj))
+        }
+        catch (error) {
+            Message.error('发送高拍仪请求失败')
+        }
+    }
+
+    // 响应ws请求
+    handleWSMessage(str) {
+        let { FuncName, result } = JSON.parse(str)
+        // 初始化
+        if (FuncName === 'camInitCameraLib') {
+            this.openDev()
+        }
+        // 拍照base64
+        else if (FuncName === 'CaptureEncodeBase64') {
+            this.options.onCaptureEncodeBase64(result)
+        }
+    }
+}
--- src/views/family-sms/sms-send/components/sms-send-modal.vue
+++ src/views/family-sms/sms-send/components/sms-send-modal.vue
@@ -28,26 +28,45 @@
                         {{ smsContent }}
                     </template>
                     <template v-if="modalData.status == 1">
-                        <m-upload-img 
-                            v-model="imgUrl"
-                            :imgStyle="{'width': '300px', 'height': '400px'}" />
+                        <div class="deli-video" v-if="!imgUrl">
+                            <img id="deliImg" alt="高拍仪" style="width: 506px; height: 380px">
+                        </div>
+                        <m-img-viewer
+                            v-else
+                            style="width: 506px; height: 380px"
+                            :url="imgUrl"
+                            title="短信内容"
+                            isRequired
+                            :isLazy="false"
+                        />
                     </template>
                 </dd>
             </dl>
         </div>
         <span slot="footer" class="dialog-footer">
+            <template v-if="modalData.status == 1">
+                <el-button v-if="!imgUrl" type="primary" @click="handleCapture" :disabled="!isDevSuccess || isCapture" >拍 照</el-button>
+                <el-button v-else type="primary" @click="handleCancel">重新拍照</el-button>
+            </template>
+            <el-button type="primary" @click="handleSmsSend">发 送</el-button>
             <el-button @click="handleClose">取 消</el-button>
-            <el-button type="primary" @click="handleSmsSend">确 定</el-button>
         </span>
     </el-dialog>
 </template>
 
 <script>
-import { toRefs, ref, reactive, watch } from '@vue/composition-api'
+import { toRefs, ref, reactive, watch, onMounted, onUnmounted } from '@vue/composition-api'
 import http from '@/service'
 import { smsSendTemplate } from '@/common/constants/const'
-import { Message } from 'element-ui';
+import { Message } from 'element-ui'
+import highBeatMeter from './high-beat-meter.vue'
+import Deli from '@/utils/deliUtil'
+import { dataURLtoFile } from '@/utils/helper'
+
 export default {
+    components: {
+        highBeatMeter
+    },
     props: {
         value: Boolean,
         input: Function,
@@ -58,10 +77,49 @@ export default {
         const smsVisible = ref(false)
         const { value, modalData } = toRefs(props)
         const smsContent = ref('')
-        const imgUrl = ref('')
+        const imgUrl = ref('1')
+        const deliDevSrc = ref('')
+        const isCapture = ref(false)
+        const isDevSuccess = ref(false)
+        let deliIns = null
+
+        // 创建得力高拍仪实例
+        if (!deliIns) {
+            deliIns = new Deli({
+                // 高拍仪连接后回调
+                onCaptureWSMessage(b) {
+                    if (smsVisible.value && !imgUrl.value) {
+                        // deliDevSrc.value = `data:image/png;base64,${ b }`
+                        document.getElementById("deliImg").src = `data:image/png;base64,${ b }`
+                    }
+                },
+                // 高拍仪拍照回调
+                onCaptureEncodeBase64(re) {
+                    isCapture.value = false
+                    // imgUrl.value = deliDevSrc.value = `data:image/png;base64,${ re }`
+                    http.sendMessageFile({
+                        file: dataURLtoFile(`data:image/png;base64,${ re }`)
+                    })
+                },
+                // 高拍仪连接成功
+                onWsopen() {
+                    isDevSuccess.value = true
+                },
+                // 高拍仪连接失败
+                onWsError() {
+                    isDevSuccess.value = false
+                }
+            })
+        }
 
         watch(value, val => {
-            val && ( imgUrl.value = '' )
+            if (val) {
+                imgUrl.value = ''
+                if (!isDevSuccess.value && modalData.value.status == 1) {
+                    Message.error('高拍仪连接失败，请重试')
+                    deliIns.load()
+                }
+            }
             smsVisible.value = val
         })
 
@@ -72,17 +130,61 @@ export default {
             }
         })
 
+        onMounted(() => {
+            deliIns.load()
+        })
+
+        onUnmounted(() => {
+            deliIns.unload()
+            deliIns = null
+        })
+
+        // 关闭弹窗
         const handleClose = () => {
             emit('input', false)
         }
 
+        // 连接搞拍仪-拍照
+        const handleCapture = () => {
+            if (isCapture.value) {
+                return
+            }
+            isCapture.value = true
+            try {
+                deliIns.send({ FuncName: 'CaptureEncodeBase64' })
+            } catch (error) {
+                isCapture.value = false
+                Message.error('拍照失败，请重试')
+            }
+        }
+
+        // 连接高拍仪-重新拍照
+        const handleCancel = () => {
+            if (imgUrl.value) {
+                imgUrl.value = ''
+            }
+        }
+
+        // 发送亲情短信
         // status 1发送普通短信 2发送开通亲情提醒短信 3发送缴费提醒短信 4短信已达上限
         // messageType 1文字 2图片 3开通提醒短信 4余额不足提醒
         const handleSmsSend = async () => {
             let { prisonerId, criminalNumber, prisonerName, familyId, familyPhone, familyName, relationship, status } = modalData.value
-            if ( status == 1 && !imgUrl.value ) {
-                Message.error('请上传短信内容')
-                return
+            if (status == 1) {
+                if (!imgUrl.value) {
+                    Message.error('请上传短信内容')
+                    return
+                }
+                else {
+                    try {
+                       let { url } = await http.sendMessageFile({
+                           file: dataURLtoFile(imgUrl.value)
+                       })
+                        imgUrl.value = url
+                    } catch (error) {
+                        this.$message.error('上传短信内容失败，请重试')
+                    }
+                }
             }
             try {
                 let res = await http.sendMessage({
@@ -97,13 +199,12 @@ export default {
                     messageType: status == 1 ? 2 : ( status == 2 ? 3 : (status == 3 ? 4 : 1) ),
                     isPrisonerSend: 1
                 })
-                console.log('handleSmsSend====', res)
                 if (res) {
                     handleClose()
                     props.queryMethod()
                 }
             } catch (error) {
-                this.$message.error('上传短信内容失败，请重试')
+                this.$message.error('发送短信内容失败，请重试')
             }
         }
 
@@ -112,7 +213,11 @@ export default {
             smsContent,
             imgUrl,
             handleSmsSend,
-            handleClose
+            handleClose,
+            deliDevSrc,
+            handleCapture,
+            isCapture,
+            isDevSuccess
         }
     }
 }
@@ -137,6 +242,11 @@ export default {
             &:last-child {
                 width: 100%;
             }
+        }
+        .deli-video {
+            width: 506px;
+            height: 380px;
+            background-color: #F5F7FA;
         }
     }
 </style>
