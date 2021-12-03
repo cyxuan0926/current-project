517c2aca2921262def12445eb3cab3ba92e6e845
caiyang <caiyang@sinog2c.com>
2021-12-03 18:07:43 +0800

    fix: 监狱数据管理 数据导入

--- src/components/modules/excel-download.vue
+++ src/components/modules/excel-download.vue
@@ -19,7 +19,9 @@ import { helper } from '@/utils'
 export default {
   props: {
     path: String,
+
     params: Object,
+
     text: {
       type: String,
       default: '导出 Excel'
@@ -56,8 +58,9 @@ export default {
 
       const query = qs.stringify(params)
 
-      let res = await getFile(apiHost + apiPath + this.path + (query && '?' + query))
-      const { headers } = res
+      const res = await getFile(apiHost + apiPath + this.path + (query && '?' + query))
+
+      const { headers } = res || {}
 
       const originFileName = headers['content-disposition'] && decodeURIComponent(headers['content-disposition'].replace(/^attachment;filename=/g, ''))
 
