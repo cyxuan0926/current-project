b9593cb829833812381cca450b9937454073b9e4
zhouyujin <18513385282@163.com>
2022-01-07 15:07:22 +0800

    no message

--- src/views/check-meeting/meeting-deploy/meeting-deploy.vue
+++ src/views/check-meeting/meeting-deploy/meeting-deploy.vue
@@ -220,7 +220,6 @@ export default {
       abnormalCallDurationSwitch: true,
       multistageExamine: false,
       familyrelations: false,
-      primary:"",
       family: "",
       content: [],
       formData: {
