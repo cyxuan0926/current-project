4cf4121cc071a1d8443b89842fe8115917f807d9
cyxuan0926 <840473997@qq.com>
2022-01-11 16:56:59 +0800

    fix: 广告管理列表 监狱名称字段为jailName 脱敏字段去除adservicesName/typeName

--- src/components/modules/table-new.vue
+++ src/components/modules/table-new.vue
@@ -103,7 +103,7 @@ export default {
       const regPrisonerNumber = /^(?!phone|terminal|room).*number$/i
 
       // 姓名之类的规则 这些因为有交集 所以需要主动添加
-      const regName = /^(?!province|jail|diplomats|org|account|real|position|sourceJail|targetJail|prisonConfig|prisonArea|user|full|city|county|terminal).*name$/i // 这些因为有冲突 重复 不能统一处理
+      const regName = /^(?!province|jail|diplomats|org|account|real|position|sourceJail|targetJail|prisonConfig|prisonArea|user|full|city|county|terminal|adservices|type).*name$/i // 这些因为有冲突 重复 不能统一处理
 
       // uuid
       const regIdCard = /.*uuid$/i
