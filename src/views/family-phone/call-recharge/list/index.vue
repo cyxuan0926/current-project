9cd8fa3ca786dc7c9787c9253d2bfdc821c05910
zhouyujin <18513385282@163.com>
2022-04-02 09:14:05 +0800

    no message

--- src/views/family-phone/call-recharge/list/index.vue
+++ src/views/family-phone/call-recharge/list/index.vue
@@ -11,30 +11,9 @@
       v-bind="routeProps"
       :isSearchLimit="isSearchLimit"
     >
-      <!-- <template #ygSearchAppendPreSlots
-        ><el-button type="primary" v-if="$isSuperAdmin" @click="onNewFamily"
-          >新增</el-button
-        ></template
-      > -->
+     
     </m-yg-prison-content>
-    <!-- 新增按钮 -->
-    <!-- <el-dialog
-      class="authorize-dialog"
-      ref="familyInformationDialog"
-      :visible.sync="familyphonerechargeamount"
-      :close-on-click-modal="false"
-      title="亲情电话充值金额"
-      width="40%"
-      @close="onCloseFamilyInformationDialog"
-    >
-      <m-form
-        ref="familyInformationDialogForm"
-        :items="familyInformationDialogFormItems"
-        :values="familyInformationDialogFormValues"
-        @cancel="familyphonerechargeamount = false"
-        @submit="onFamilyInformationDialogFormSubmit"
-      />
-    </el-dialog> -->
+  
   </el-row>
 </template>
 
@@ -54,79 +33,19 @@ export default {
 
     const searchItems = ref(_.cloneDeep(_searchItems));
 
-    // const familyInformationDialogFormItems = ref({
-    //   buttons: ["add", "cancel"],
-    //   formConfigs: {
-    //     labelWidth: "120px",
-    //   },
-    //   jailName: {
-    //     type: "input",
-    //     label: "监狱名称",
-    //     rules: ["required"],
-    //     value: "",
-    //     clearable: true,
-    //   },
-    //   prisonerName: {
-    //     type: "input",
-    //     label: "罪犯姓名",
-    //     rules: ["required"],
-    //     value: "",
-    //     clearable: true,
-    //   },
-    //   prisonerNumber: {
-    //     type: "input",
-    //     label: "罪犯编号",
-    //     rules: ["required"],
-    //     value: "",
-    //     clearable: true,
-    //   },
-    //   vocationalResult: {
-    //     type: "input",
-    //     label: "亲情电话充值金额",
-    //     rules: ["required", "phone"],
-    //     // rules: ["required"],
-    //     value: "",
-    //     clearable: true,
-    //   },
-    //   remark: {
-    //     type: "input",
-    //     label: "备注",
-    //     // rules: ["required"],
-    //     value: "",
-    //     clearable: true,
-    //   },
-    // });
 
     const $tabs = ref("0");
 
     const $tabItems = reactive(tabItems);
 
-<<<<<<< HEAD
-    const familyInformationDialogFormValues = ref({});
-
-    const familyphonerechargeamount = ref(false);
-=======
-    // const familyInformationDialogFormValues = ref({});
-
-    // const familyphonerechargeamount = ref(false);
->>>>>>> 57973a33586dab7f3c86b1bcdcf372f537f44d1c
-
-    // const familyInformationVisible = ref(false);
 
     const isSearchLimit = ref(true);
     // store ywt_admin账号
     const $isSuperAdmin = computed(() => store.getters.isSuperAdmin);
-    // const omponentpublic = computed(() => {
-
-    //   return {
-    //     // 设置 ywt_admin下面有导入和模版
-    //     excelUploadVisible:  $isSuperAdmin.value , //                               ,
-    //     excelDownloadVisible: $isSuperAdmin.value,
-    //   };
-    // });
+ 
     const omponentpublic = ref({
       // 设置 ywt_admin下面有导入和模版
-      excelUploadVisible: $isSuperAdmin.value, //                               ,
+      excelUploadVisible: $isSuperAdmin.value,
       excelDownloadVisible: $isSuperAdmin.value,
     });
 
@@ -160,32 +79,7 @@ export default {
 
     const { routeProps } = useRouteProps();
 
-    // async function onNewFamily() {
-    //   familyphonerechargeamount.value = true;
-    // }
-    // 新增接口
-    // async function onFamilyInformationDialogFormSubmit(data) {
-    //   if (data) {
-    //     let res = await http.familyforAdd(data);
-    //     if (res === undefined) return;
-    //     setTimeout(() => {
-    //       this.onCloseFamilyInformationDialog();
-    //       $callRechargeParent.value && $callRechargeParent.value.initData();
-    //     }, 1000);
-    //   } else {
-    //     this.$message({
-    //       showClose: true,
-    //       message: "未编辑信息，无须提交审批！",
-    //       duration: 2000,
-    //       type: "error",
-    //     });
-    //   }
-    // }
-    // 关闭对话框
-    // async function onCloseFamilyInformationDialog() {
-    //   this.$refs.familyInformationDialogForm &&
-    //     this.$refs.familyInformationDialogForm.onCancel();
-    // }
+   
 
     watch($tabs, (val) => {
       if (val == "0") {
@@ -210,14 +104,7 @@ export default {
       routeProps,
       $isSuperAdmin,
       omponentpublic,
-      isSearchLimit,
-      // familyphonerechargeamount,
-      // onNewFamily,
-      // familyInformationVisible,
-      // familyInformationDialogFormItems,
-      // familyInformationDialogFormValues,
-      // onFamilyInformationDialogFormSubmit,
-      // onCloseFamilyInformationDialog,
+      isSearchLimit
     };
   },
 };
